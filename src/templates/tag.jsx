bd2fe70d3c9b4e55c00c0e6c410a09e385aea7b0
Jonathan Nicholas <jojonicho181@gmail.com>
2020-08-04 22:21:21 +0700

    :art(templates): updated top margin on /tags/{tag} page

--- src/templates/tag.jsx
+++ src/templates/tag.jsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Helmet } from 'react-helmet';
-import { Layout } from 'layouts';
-import { BlogList } from 'components';
+import React from "react";
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
+import { Layout } from "layouts";
+import { BlogList } from "components";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  margin-top: 3rem;
+`;
 
 const Tag = ({ pageContext }) => {
   const { posts, tagName } = pageContext;
   return (
     <Layout>
       <Helmet title={`${tagName} | Jojonicho`} />
-      {posts.map((post) => (
-        <BlogList
-          key={post.id}
-          cover={post.frontmatter.cover.childImageSharp.fluid}
-          path={post.frontmatter.path}
-          title={post.frontmatter.title}
-          date={post.frontmatter.date}
-          tags={post.frontmatter.tags}
-          excerpt={post.excerpt}
-        />
-      ))}
+      <Container>
+        {posts.map((post) => (
+          <BlogList
+            key={post.id}
+            cover={post.frontmatter.cover.childImageSharp.fluid}
+            path={post.frontmatter.path}
+            title={post.frontmatter.title}
+            date={post.frontmatter.date}
+            tags={post.frontmatter.tags}
+            excerpt={post.excerpt}
+          />
+        ))}
+      </Container>
     </Layout>
   );
 };
