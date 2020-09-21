6e0728a77044b50c4127990b1030b49bf6683f75
Jonathan Nicholas <jojonicho181@gmail.com>
2020-09-22 01:07:52 +0700

    fixed image meta data routing

--- config/site.js
+++ config/site.js
@@ -6,10 +6,10 @@ module.exports = {
   url: "https://jojonicho.gtsb.io", // Domain of your site. No trailing slash!
   siteUrl: "https://jojonicho.gtsb.io/", // url + pathPrefix
   siteLanguage: "en", // Language Tag on <html> element
-  logo: "public/static/logo/profpic.png", // Used for SEO
-  banner: "public/static/logo/jojonicho.svg",
+  logo: "/logo/profpic.png", // Used for SEO
+  banner: "/logo/jojonicho.svg",
   // JSONLD / Manifest
-  favicon: "public/static/logo/profpic.png", // Used for manifest favicon generation
+  favicon: "static/logo/profpic.png", // Used for manifest favicon generation
   shortName: "jojonicho", // shortname for manifest. MUST be shorter than 12 characters
   author: "Jonathan", // Author for schemaORGJSONLD
   themeColor: "#3e7bf2", // '#1C2833'
